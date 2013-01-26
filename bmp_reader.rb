0db47b5660d761d26844489ba9343dd081f5d331
Holden Cox <segrived@gmail.com>
2013-01-26 00:03:47 +0000

    Начальная поддержка различных форматов заголовков

--- bmp_reader.rb
+++ bmp_reader.rb
@@ -1,11 +1,8 @@
+# encoding: UTF-8
+
 require './global'
 
 class BMPReader
-
-    FILE_HEADER = 'A2LSSL'
-    MAP_HEADER  = 'LLLSSLLLLLL'
-    FILE_HEADER_LENGTH = 14
-    MAP_HEADER_LENGTH  = 40
 
     attr :file_name, true
     attr :pixels, true
@@ -13,26 +10,31 @@ class BMPReader
     attr :height
     attr :bpp
 
+    @map_header = nil
+
     def initialize(file_name = nil, autoread = false)
         @file_name = file_name
-        if autoread then read_file end
+        if autoread then read end
     end
 
     # Читает заголовки файлов и массив пикселов
-    def read_file
+    def read
         # Заголовок файла и изображения
-        @file_header, @map_header = read_file_header, read_map_header
+        @file_header = read_file_header
+        @map_header = read_map_header
+
         # Присваивание значений переменным класса
         @width, @height, @bpp = @map_header[:width], @map_header[:height], @map_header[:bpp]
 
         f = File.open(file_name, 'r')
         # Переходим к началу изобраажения
         f.seek(@file_header[:off_bits], IO::SEEK_SET)
+
         @pixels, line = [[]], 0
-        # Количество байт для выравнивания
-        alignment = @width * 3 % 4
-        # Теоретически должно помочь обрабатывать данные с bpp=32
+        # Количество байт на пиксель
         bytes_per_pixel = bpp / 8
+        # Количество байт для выравнивания
+        alignment = @width * bytes_per_pixel % 4
         bytes_per_line = bytes_per_pixel * width
         # Чтение изображения по линиям
         while (buffer = f.read(bytes_per_line)) do
@@ -62,12 +64,10 @@ class BMPReader
 
         # Заголовок файла
         io.write [fh.header, fh.file_size, fh.reserved1,
-            fh.reserved2, fh.off_bits].pack(FILE_HEADER)
+            fh.reserved2, fh.off_bits].pack(FILE_HEADER_FORMAT)
 
         # Заголовок изображения
-        io.write [mh.size, mh.width, mh.height, mh.planes, 
-            mh.bpp, mh.compression, mh.image_size, mh.xppm,
-            mh.yppm,mh.clr_used, mh.clr_important].pack(MAP_HEADER)
+        io.write @map_header.generate_header
 
         # Количество байт для выравнивания
         alignment = width * 3 % 4
@@ -81,27 +81,96 @@ class BMPReader
         }
     end
 
-    private
-
     # Возвращает структуру с заголовкой файла
     def read_file_header
         f = File.open(file_name, 'r')
         header, file_size, reverved1, reserved2, off_bits =
-            f.read(FILE_HEADER_LENGTH).unpack(FILE_HEADER)
+            f.read(FILE_HEADER_LENGTH).unpack(FILE_HEADER_FORMAT)
         f.close
         BmpFileHeader.new(header, file_size, reverved1, reserved2, off_bits)
     end
 
-    # Возвращает структуру с заголоком изображения
     def read_map_header
-        f = File.open(file_name, 'r')
-        f.seek(FILE_HEADER_LENGTH)
-        size, width, height, planes, bpp, compression,
-        image_size, xppm, yppm, clr_used, clr_important =
-            f.read(MAP_HEADER_LENGTH).unpack(MAP_HEADER)
-        f.close
-        BmpMapHeader_BITMAPINFOHEADER.new(size, width, height, planes, bpp, compression,
-            image_size, xppm, yppm, clr_used, clr_important)
+        f = File.open(@file_name, 'r')
+        f.seek(FILE_HEADER_LENGTH, IO::SEEK_SET)
+        length = f.read(4).unpack('L')[0]
+        inst = case length
+            when 12 then ::BmpMapHeaderOCH.new
+            when 40 then ::BmpMapHeaderBIH.new
+            else raise 'Неизвестный формат BMP-файла'
+        end
+        inst.read_header f
+        inst
     end
 
+end
+
+
+class BmpMapHeader
+
+    attr :fields
+
+    def initialize
+        @fields = {}
+    end
+
+    def [](field)
+        @fields[field]
+    end
+
+    def read_header(io)
+        raise NotImplementedError
+    end
+
+    def generate_header
+        raise NotImplementedError
+    end
+
+end
+
+class BmpMapHeaderBIH < BmpMapHeader
+
+    LENGTH = 40
+    FORMAT = 'LLLSSLLLLLL'
+
+    def read_header(io)
+        io.seek(FILE_HEADER_LENGTH, IO::SEEK_SET)
+        size, width, height, planes, bpp, compression, image_size,
+        xppm, yppm, clr_used, clr_important = io.read(LENGTH).unpack(FORMAT)
+        @fields = { :size => size, :width => width, :height => height,
+            :planes => planes, :bpp => bpp, :compression => compression,
+            :image_size => image_size, :xppm => xppm, :yppm => yppm,
+            :clr_used => clr_used, :clr_important => clr_important
+        }
+    end
+
+    def generate_header
+        [@fields[:size], @fields[:width], @fields[:height],
+            @fields[:planes], @fields[:bpp], @fields[:compression],
+            @fields[:image_size], @fields[:xppm], @fields[:yppm],
+            @fields[:clr_used], @fields[:clr_important]
+        ].pack(FORMAT)
+    end
+end
+
+class BmpMapHeaderOCH < BmpMapHeader
+
+    header = {
+        :length => 12,
+        :format => 'LSSSS'
+    }
+
+    def read_header(io)
+        io.seek(FILE_HEADER_LENGTH, IO::SEEK_SET)
+        size, width, height, planes, bpp = io.read(header[:length]).unpack(header[:format])
+        @fields = { :size => size, :width => width, :height => height,
+            :planes => planes, :bpp => bpp
+        }
+    end
+
+    def generate_header
+        [@fields[:size], @fields[:width], @fields[:height],
+            @fields[:planes], @fields[:bpp]
+        ].pack(header[:format])
+    end
 end
